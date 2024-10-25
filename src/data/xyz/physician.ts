1fb74cbaa7b8633d27c53ef1500cc0444e398f4c
templar-ajay <templar.ajay@gmail.com>
2024-10-25 09:49:39 +0000

    values mapped

---
+++ src/data/xyz/physician.ts
@@ -0,0 +1,7 @@
+export const map_physician = {
+  "Ali, NP, Jessica Fay": "UID_060Z6BZZDCC3FY0987Y4",
+  "Gelfand, MD, Robert": "UID_Az86426924_1451",
+  "Ghuman, MD, Damanjit": "UID_066ATE4VW2A49WWGN5KW",
+  "Kramer, MD, Rachel": "UID_VV664631032_1165",
+  "Livescu, NP, Nicole": "UID_VV370565293_2160",
+};
